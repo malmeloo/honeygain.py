80c99bbc1c213b4fa49ee3a6549bfe8651aa5295
Mike A <almeloomike@gmail.com>
2022-02-18 20:19:31 +0100

    Add LICENSE and prepare for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setup(
+    name='Honeygain.py',
+    version='1.0',
+    description='Python wrapper for the Honeygain API.',
+    long_description=long_description,
+    url='https://github.com/DismissedGuy/honeygain.py',
+
+    author='Mike A.',
+    author_email='dismissed.is.a.guy@gmail.com',
+    packages=['honeygain'],
+    license='License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent'
+    ]
+)
